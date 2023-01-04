842f24b27a9fffbf03116c04a6c9cfa7e338df1e
sloth <gkwkgo321@gmail.com>
2023-01-04 11:17:47 +0900

    1/4/23

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
+import {useState} from "react";
 function Header(props) {
   return (
     <header>
       <h1>
-        <a
-          href="/"
-          onClick={(event) => {
+        <a href="/" onClick={(event) => { 
             event.preventDefault(); //a 태그 기본동작을 방해한다!
             //onClick의 함수가 호출 되었을때 헤드의 props로 전달된 onChangeMode가 가리키는 함수를 호출해야한다.
             props.onChangeMode();
@@ -16,9 +15,8 @@ function Header(props) {
       </h1>
     </header>
   );
-}
-//Nav의 내부 안으로 들어와서 topic를 받아주기
-const Nav = (props) => {
+};
+const Nav = (props) => {//Nav의 내부 안으로 들어와서 topic를 받아주기
   //첫번째 파라미터는 props로
   const lis = []; //lis는 배열으로 선언
   for (let i = 0; i < props.topics.length; i++) {
@@ -28,7 +26,7 @@ const Nav = (props) => {
       <li key={t.id}>
         <a id={t.id} href={"/read/" + t.id} onClick={(event)=>{
           event.preventDefault();
-          props.onChangeMode(event.target.id);//id값을 얻어내야 한다.
+          props.onChangeMode(Number(event.target.id));//id값을 얻어내야 한다. 숫자로!
         }}>{t.title}</a>
       </li>
     );
@@ -49,32 +47,65 @@ const Article = (props) => {
     </article>
   );
 };
+function Create(props){
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={event=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onCreate(title, body);
+    }}>
+      <p><input type="text" name="title" placeholder=" title here" /></p>
+      <p><textarea name="body" placeholder=" body here"></textarea></p>
+      <p><input type="submit"/></p>
+    </form>
+  </article>
+};
+
 function App() {
-  //const에 담아두면 후에 바뀌지 않아서 단단한 코드가 된다.
-  const topics = [
+  const [mode, setMode] = useState('WELCOME');
+  const [id,setId]=useState(null);
+  const [nextId,setNextId]=useState(4); //id를 따로 관리하기 위해 새로운 값은 4부터 시작
+  const [topics, setTopics] = useState ([
     //각각의 정보는 여러개니까 배열에 담는다.
     { id: 1, title: "html", body: "html is ..." },
     { id: 2, title: "css", body: "css is ..." },
     { id: 3, title: "javascript", body: "javascript is ..." },
-  ];
+  ]);
+
+  let content = null; //변하게 될 값이니까 let으로 설정한다.
+  if (mode === 'WELCOME') {
+    content = <Article title="반가워요!" body="안녕하세요~" />
+  }else if (mode === 'READ') {
+    let title, body=null;
+    for(let i = 0; i < topics.length; i++) {
+      if(topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+    };}
+    content = <Article title={title} body={body} />
+  }else if (mode === 'CREATE'){
+    content = <Create onCreate={(_title, _body)=>{
+      const newTopic = {id:nextId, title:_title, body:_body};
+      const newTopics = [...topics]
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId+1);
+    }}/>
+  }
   return (
     <div className="App">
-      <Header
-        title="웹"
-        onChangeMode={() => {
-          alert("Header");
-        }}
-      />
-      <Nav
-        topics={topics}
-        onChangeMode={(id) => {
-          //함수에 파라미터로 id 값이 전달 되었으면 한다.
-          alert(id); //그리고 받아온 id 파라미터를 출력한다.
-        }}
-      />
-      {/** 위에서 만든 topic배열을 내부의 prop으로 전달한다. */}
-      <Article title="반가워요!" body="안녕하세요~" />
+      <Header title="웹" onChangeMode={() => {setMode('WELCOME');}}></Header>
+      <Nav topics={topics} onChangeMode={(_id) => {
+        setMode('READ');
+        setId(_id); //내부 컨포넌트의 글을 클릭해서 id 값이 바뀌면 컨포넌트가 새로 시작되면서 바뀐 id값으로 적용된다.
+      }}></Nav>
+      {content} 
+      <a href="/create" onClick={event=>{ event.preventDefault(); setMode('CREATE');}}>Create</a>
     </div>
   );
-}
+};
 export default App;
